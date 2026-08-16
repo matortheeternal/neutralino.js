75d1c98112cf03bbcc3d0ba0da1f7e1679c383ea
Mator <mator@noreply.codeberg.org>
2026-08-16 13:38:29 -0700

    migrate to eslint flat file format

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default [
+    {
+        ignores: ['node_modules/', 'dist/'],
+    },
+
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+
+    {
+        files: ['**/*.ts'],
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'off',
+        },
+    },
+
+    {
+        files: ['*.mjs', '*.js'],
+        languageOptions: {
+            globals: {
+                ...globals.node,
+            },
+        },
+    },
+
+    prettier,
+];
